d8aabfddd35e183bc7b587f451c40b4523427a98
Icebob <mereg.norbert@gmail.com>
2017-07-07 15:03:10 +0200

    update deps, try caching

--- blog/moleculer.config.js
+++ blog/moleculer.config.js
@@ -6,5 +6,6 @@ module.exports = {
 	// Append hostname to nodeID. It will be unique when scale up instances in Docker
 	nodeID: (process.env.NODEID ? process.env.NODEID + "-" : "") + os.hostname().toLowerCase(),
 	metrics: true,
-	statistics: true
+	statistics: true,
+	//cacher: true
 };
